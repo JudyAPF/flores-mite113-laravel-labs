20d1c4ce53cf21e8f5bf4e017cc2e3df1ae067ad
Judy Ann Flores <floresjudyannp@gmail.com>
2026-09-05 12:28:04 +0800

    change default route

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 /*
 |--------------------------------------------------------------------------
 | Lab Activity 1 - Student Profile
@@ -14,7 +10,7 @@ Route::get('/', function () {
 | GET /profile  ->  ProfileController@show  ->  resources/views/profile.blade.php
 | The route is NAMED "profile.show" so we can link to it with route('profile.show').
 */
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+Route::get('/', [ProfileController::class, 'show'])->name('profile.show');
 
 /*
 | EXTENSION / CHALLENGE: /profile/{name} reads the student from MySQL.
